075e1ef5a5ddf1161d71e47496e0af03a66940d5
Brayan <Brayan>
2016-10-31 11:32:07 -0500

    Initial commit

---
+++ app/src/main/java/android/modulo8/brayan/modulo12/MainActivity.java
@@ -0,0 +1,145 @@
+package android.modulo8.brayan.modulo12;
+
+
+import android.os.Bundle;
+
+import android.support.v7.app.ActionBarActivity;
+
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+
+import android.util.Base64;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+import android.util.Log;
+
+import com.google.android.gms.ads.doubleclick.PublisherAdRequest;
+import com.google.android.gms.ads.doubleclick.PublisherAdView;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import android.content.pm.Signature;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private CallbackManager cM;
+    private LoginButton lB;
+    private PublisherAdView publisherAdView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        cM = CallbackManager.Factory.create();
+
+        setContentView(R.layout.activity_main);
+
+        publisherAdView = (PublisherAdView) findViewById(R.id.ad_view);
+        PublisherAdRequest adRequest = new PublisherAdRequest.Builder().build();
+        publisherAdView.loadAd(adRequest);
+
+        lB = (LoginButton)findViewById(R.id.login_facebook);
+
+        lB.registerCallback(cM, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+                Toast.makeText(MainActivity.this, "¡Inicio de sesión exitoso!", Toast.LENGTH_LONG).show();
+
+            }
+
+            @Override
+            public void onCancel() {
+
+                Toast.makeText(MainActivity.this, "¡Inicio de sesión cancelado!", Toast.LENGTH_LONG).show();
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+                Toast.makeText(MainActivity.this, "¡Inicio de sesión NO exitoso!", Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+    }
+
+    public void getFbKeyHash(String packageName){
+
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    packageName, PackageManager.GET_SIGNATURES);
+            for(Signature signature : info.signatures){
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash :", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+                System.out.println("KeyHash: " + Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+
+        } catch (PackageManager.NameNotFoundException e){
+
+        }catch (NoSuchAlgorithmException e){
+
+        }
+
+    }
+    protected void onActivityResult(int reqCode, int resCode, Intent i){
+        cM.onActivityResult(reqCode, resCode, i);
+    }
+//Banner
+@Override
+protected void onDestroy() {
+    if(publisherAdView != null){
+        publisherAdView.destroy();
+    }
+    super.onDestroy();
+}
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(publisherAdView != null){
+            publisherAdView.resume();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        if(publisherAdView != null){
+            publisherAdView.pause();
+        }
+        super.onPause();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
